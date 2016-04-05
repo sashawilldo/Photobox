3fbd4c6b548ec97ff5757ebe563d563db593f50d
sashawilldo <yishingw@icloud.com>
2016-04-05 22:31:48 +0800

    Photobox

---
+++ Photobox/PhotoboxTests/PhotoboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoboxTests.swift
+//  PhotoboxTests
+//
+//  Created by yi shing wong on 4/4/2016.
+//  Copyright © 2016 yi shing wong. All rights reserved.
+//
+
+import XCTest
+@testable import Photobox
+
+class PhotoboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
